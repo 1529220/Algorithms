ee73ef82774ba053541193db82fb83e861b69fab
laura <1529220@my.brunel.ac.uk>
2017-03-19 20:00:54 +0000

    Add files via upload

---
+++ main/java/com/example/nova/myapplication/Timetable.java
@@ -0,0 +1,24 @@
+package com.example.nova.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import static android.R.id.message;
+
+/**
+ * Created by laura on 19/03/2017.
+ */
+
+public class Timetable {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.timetable);
+
+        // Get the Intent that started this activity and extract the string
+
+
+        // Capture the layout's TextView and set the string as its text
+
+    }
+}
